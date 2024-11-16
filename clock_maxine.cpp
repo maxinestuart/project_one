4c0c78e95acb1815329e99b8f38821995805c338
Maxine Stuart <maxinejmax@gmail.com>
2024-11-16 17:48:46 -0500

    add all function fragments

--- clock_maxine.cpp
+++ clock_maxine.cpp
@@ -8,12 +8,17 @@
 #include <string>
 using namespace std;
 
-unsigned int getMenuChoice(unsigned int maxChoice) {
+unsigned int getMenuChoice(unsigned int maxChoice) { // FIXME: handles correct menu choices
     int menuChoice;
 
     cin >> menuChoice;
 
     return 0;
+}
+
+string nCharString(size_t n, char c) { // FIXME: Returns a string of length n, each character a c.
+
+    return "*"; 
 }
 
 std::string twoDigitString(unsigned int n) { // returns numbers as 2-digit strings
@@ -29,7 +34,42 @@ std::string twoDigitString(unsigned int n) { // returns numbers as 2-digit strin
     return result;
 }
 
-void mainMenu() {
+string formatTime24(unsigned int h, unsigned int m, unsigned int s) { // FIXME: return time as hh:mm:ss
+
+    return "hh:mm:ss";
+}
+
+string formatTime12(unsigned int h, unsigned int m, unsigned int s) { // FIXME: return time as hh:mm:ss
+
+    return "12:00:00 A M";
+}
+
+void printMenu(char * strings[], unsigned int numStrings, unsigned char width) { // FIXME: print formatted menu
+
+    return;
+}
+
+void displayClocks(unsigned int h, unsigned int m, unsigned int s) { // FIXME: print clocks
+
+    return;
+}
+
+void addOneSecond() { // FIXME: add 1 second
+
+    return;
+}
+
+void addOneMinute() { // FIXME: add 1 minute
+
+    return;
+}
+
+void addOneHour() { // FIXME: add 1 hour
+
+    return;
+}
+
+void mainMenu() { // FIXME: repeats getting the user's choice and taking the appropriate action until the user chooses 4 for exit
 
     getMenuChoice(4);
 
