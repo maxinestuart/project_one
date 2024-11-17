438c162e6b8eb756c7bbea2d63481f8bba84f23f
Maxine Stuart <maxinejmax@gmail.com>
2024-11-17 08:53:51 -0500

    output menu function and debugging

--- clock_maxine.cpp
+++ clock_maxine.cpp
@@ -9,50 +9,6 @@
 #include <limits>
 using namespace std;
 
-void clockSetup() { // print welcome and set initial time from input
-    int hour = 0;
-    int minute = 0;
-    int second = 0;
-
-    // validate inputs for hour, minute, and second
-    cout << "Welcome! Please enter the time for initial clock setup." << endl;
-    cout << "Enter hour:" << endl; 
-    while (!(cin >> hour) || hour < 0 || hour > 24) {
-        cout << "Please enter a valid integer between 0 and 24:" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    cout << "Enter minute:" << endl; 
-    while (!(cin >> minute) || minute < 0 || minute > 59) {
-        cout << "Please enter a valid integer between 0 and 59:" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    cout << "Enter second:" << endl; 
-    while (!(cin >> second) || second < 0 || second > 59) {
-        cout << "Please enter a valid integer between 0 and 59:" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-
-    cout << hour << minute << second; // FIXME: return/output what? 
-}
-
-unsigned int getMenuChoice(unsigned int maxChoice) { // FIXME: handles correct menu choices
-    int menuChoice;
-
-    cin >> menuChoice;
-
-    return 0;
-}
-
-string nCharString(size_t n, char c) { // Returns a string of length n, each character a c.
-    string widthStars;
-    
-    widthStars.append(n, c);
-
-    return widthStars; 
-}
 
 std::string twoDigitString(unsigned int n) { // returns numbers as 2-digit strings
     std::string result;
@@ -95,7 +51,72 @@ string formatTime12(unsigned int h, unsigned int m, unsigned int s) { // return 
     return time12Hr;
 }
 
-void printMenu(char * strings[], unsigned int numStrings, unsigned char width) { // FIXME: print formatted menu
+void clockSetup() { // print welcome and set initial time from input
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+
+    // validate inputs for hour, minute, and second
+    cout << "Welcome! Please enter the time for initial clock setup." << endl;
+    cout << "Enter hour:" << endl; 
+    while (!(cin >> hour) || hour < 0 || hour > 24) {
+        cout << "Please enter a valid integer between 0 and 24:" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Enter minute:" << endl; 
+    while (!(cin >> minute) || minute < 0 || minute > 59) {
+        cout << "Please enter a valid integer between 0 and 59:" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Enter second:" << endl; 
+    while (!(cin >> second) || second < 0 || second > 59) {
+        cout << "Please enter a valid integer between 0 and 59:" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << endl;
+    cout << "You entered: " << formatTime24(hour, minute, second) << endl; // FIXME: return/output what? 
+}
+
+const char** getMenuItems(unsigned int &numStrings) {
+    static const char *menuItems[] = {"Add One Hour", "Add One Minute", "Add One Second", "Exit Program"};
+    numStrings = 4;
+    return menuItems;
+}
+
+unsigned int getMenuChoice(unsigned int maxChoice) { // FIXME: handles correct menu choices
+    int menuChoice;
+
+    cin >> menuChoice;
+
+    return 0;
+}
+
+string nCharString(size_t n, char c) { // Returns a string of length n, each character a c.
+    string widthStars;
+    
+    widthStars.append(n, c);
+
+    return widthStars; 
+}
+
+void printMenu(const char * strings[], unsigned int numStrings, unsigned char width) { // FIXME: print formatted menu
+    cout << nCharString(width, '*') << endl;
+    for (unsigned int i = 0; i < numStrings; ++i) {
+        cout << "* " << i + 1 << " - " << strings[i];
+
+        size_t itemLength = 2 + to_string(i).length() + 3 + string(strings[i]).length(); // length of line content
+        size_t spaceCount = (width > itemLength) ? (width - itemLength - 1) : 0; // counts spaces needed 
+        cout << nCharString(spaceCount, ' ');
+        cout << "*" << endl;
+        if ((i + 1) < numStrings) { // empty lines except after last menu item
+            cout << endl;
+        }
+    }
+    cout << nCharString(width, '*') << endl;
 
     return;
 }
@@ -128,7 +149,11 @@ void mainMenu() { // FIXME: repeats getting the user's choice and taking the app
 }
 
 int main() { // FIXME: main logic
+    unsigned int numStrings;
+    const char **menuItems = getMenuItems(numStrings);
+
     clockSetup();
+    printMenu(menuItems, numStrings, 27);
 
     cout << twoDigitString(33) << endl; // FIXME: test output
 
